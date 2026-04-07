02ee6ab63c43f42b46fd178e92579bbd16559b93
ArmandOlteanu <Armand.Olteanu@uvm.edu>
2026-04-07 18:55:29 -0400

    homework 9

--- r_scripts/SQL_Lecture.R
+++ r_scripts/SQL_Lecture.R
@@ -1,2 +1,18 @@
 #basics of SQL
 
+
+library(tidyverse)
+library(sqldf)
+
+install.packages("sqldf")
+
+# read in the dataset
+
+species_clean<-read.csv("site_by_species.csv")
+
+read.csv("/Users/armand/Desktop/project/data/site_by_species.csv")
+
+
+setwd()
+
+
